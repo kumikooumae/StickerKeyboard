13563a606403d812b0355f6f3ee6397baf329aa3
Oumae Kumiko <37971934+kumikooumae@users.noreply.github.com>
2019-01-30 01:39:40 +0800

    Keyboard layout

---
+++ app/src/main/java/kumiko/stickerkeyboard/IMEService.java
@@ -0,0 +1,16 @@
+package kumiko.stickerkeyboard;
+
+import android.app.Service;
+import android.content.Intent;
+import android.inputmethodservice.InputMethodService;
+import android.inputmethodservice.KeyboardView;
+import android.os.IBinder;
+import android.view.View;
+
+public class IMEService extends InputMethodService {
+    @Override
+    public View onCreateInputView() {
+
+
+    }
+}
---
+++ app/src/main/java/kumiko/stickerkeyboard/StickerAdapter.java
@@ -0,0 +1,6 @@
+package kumiko.stickerkeyboard;
+
+import android.support.v7.widget.RecyclerView;
+
+public class StickerAdapter extends RecyclerView.Adapter {
+}
